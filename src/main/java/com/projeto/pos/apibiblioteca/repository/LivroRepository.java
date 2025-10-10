7390911433052f5818dae504162f9c4bff7dcaba
Pedro Henrique <pedrohaugusto12@gmail.com>
2025-10-10 16:36:36 -0300

    feat: adicionando logs na api

--- src/main/java/com/projeto/pos/apibiblioteca/repository/LivroRepository.java
+++ src/main/java/com/projeto/pos/apibiblioteca/repository/LivroRepository.java
@@ -1,9 +1,11 @@
 package com.projeto.pos.apibiblioteca.repository;
 
 import com.projeto.pos.apibiblioteca.model.Livro;
+import com.projeto.pos.apibiblioteca.model.dtos.LivroDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+    LivroDto findByTitulo(LivroDto livroDto);
 }
